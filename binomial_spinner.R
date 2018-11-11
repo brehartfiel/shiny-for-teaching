af56535f414597cca2d88caaf3f32689e18c0e3b
brehartfiel <bre.hartfiel@gmail.com>
2018-11-11 17:29:17 -0600

    Spinner tab works. Simulations tab half works. Table of successes & failures from spinner works.  Additional binomial simulation data does not work. Summary table works if sims = 1.  If additional binomial simulation data is needed, histogram errors out.

--- binomial_spinner.R
+++ binomial_spinner.R
@@ -7,55 +7,31 @@ library(shiny)
 
 # Define UI for application that plots random distributions
 ui <- fluidPage(
-  
-  # Application title
   headerPanel("Binomial Simulation and Spinner"),
-  
-  # Sidebar with a slider input for number of observations
   sidebarPanel(
+    sliderInput("psuccess", "Probability of Success:", min = 0, max = 1, value = 0, step = .01),
+    sliderInput("n", "Sample Size:", min = 1, max = 30, value = 1, step=1.0),
+    sliderInput("sims", "Number of Repeated Samples:", min = 1, max = 100, value = 1, step= 1.0),
     
-    # Specify the probaility of success
-    sliderInput("psuccess", "Probability of Success:", 
-                min = 0, max = 1, value = 0, step = .01),
-    #Specify the sample size
-    sliderInput("n", "Sample Size:",
-                min = 1, max = 30, value = 1, step=1.0),
-    
-    # Specify the parameter
-    sliderInput("sims", "Number of Repeated Samples:",
-                min = 1, max = 100, value = 1, step= 1.0),
-    
-    # Specify if the user wants to show the p-value testing information
-    checkboxInput("check",
-                  label = strong("Test a specific value"),
-                  value = FALSE),
-    
-    conditionalPanel(
-      condition = "input.check == true",
-      
-      numericInput("obs", "Observed Count:", min = 0, value = 1.0),
-      
-      selectInput("test", "Choose a type of test:", 
-                  list(" " = "blank",
-                       "Left-tailed" = "left",
-                       "Right-tailed" = "right", 
-                       "Two-tailed" = "two"))    
+    checkboxInput("check", label = strong("Test a specific value"), value = FALSE),
+      conditionalPanel(
+        condition = "input.check == true",
+          numericInput("obs", "Observed Count:", min = 0, value = 1.0),
+          selectInput("test", "Choose a type of test:", 
+                    list(" " = "blank",
+                         "Left-tailed" = "left",
+                         "Right-tailed" = "right", 
+                         "Two-tailed" = "two"))    
     )
   ),
-  
-  # where the plots get displayed
   mainPanel(
-    
     tabsetPanel(
-      tabPanel("Spinner",
-               # displaying piPlot under the Spinner tab
-               plotOutput("circlePlot", height = "509px", width = "500px")
+      tabPanel("Spinner", plotOutput("circlePlot", height = "500px", width = "500px")
       ), 
-      tabPanel("Simulations",
-               # displaying the number of successes/failures in the first simulation
-               h4("One simulation from the spinner"),
+      tabPanel("Simulations", 
+               h4("One simulation from the spinner"), 
                tableOutput("onesimTable"),
-               # displaying the remaining simulations
+               
                h4("The remaining simulations"),
                tableOutput("remainingTable")
       ),
@@ -78,182 +54,54 @@ server <- function(input, output) {
   # to be called later using myData()
   # the reactive() around the function makes it so that the lines for the spinner
   # will not change until you refresh the page
-  myData <- reactive(function() {
-    # Generate a uniform distribution from 0 to 2pi (think polar coordinates)
-    runif(n=100, min=0, max=1)
-    
-  })
+  data = isolate({runif(n=100, min=0, max=1)*(2*pi)})
+  bindata <- isolate({rbinom(1000, input$n, input$psuccess)})
   
-  binData <- reactive(function() {
-    # Generate a binomal distribution 
-    rbinom(1000, input$n, input$psuccess)
-  })
-  
-  # Generates piPlot and allows it to be reactive
   output$circlePlot <- renderPlot({
+    # setting up a plot
+    plot(0,0, xlim = c(-1,1), ylim = c(-1,1), xlab = "", ylab = "", frame.plot = FALSE, axes=FALSE)
     
-    # changing the value of pi into an x and y coordinate using polar coordinate knowledge.
-    # x = rcos(theta) and y = rsin(theta) where theta is between 0 and 2pi
-    xcart = cos(input$psuccess*2*pi)
-    ycart = sin(input$psuccess*2*pi)
+    # this sets up a series of coordinates that will be used to shade the circle
+    # starting at (0,0) so the center is the first edge point
+    # calculating cartesian corrdinates for an entire circle in increments of 1000 to be called later
+    xcart = c(0, cos(2*pi*seq(0,1,1/1000)))
+    ycart = c(0, sin(2*pi*seq(0,1,1/1000)))
     
-    # getting the data and setting the data up to be used
-    data = myData()
-    data = data.frame(data*(2*pi))
-    data = data[1:input$n,]
-    
-    # Setting up the plot area for the circle
-    plot(0,0,
-         xlim = c(-1,1), ylim = c(-1,1), 
-         xlab = "", ylab = "", frame.plot = FALSE, axes=FALSE)
-    
-    # coloring in the circle 
-    if(input$psuccess <= .25){
-      # the x coordinates for the curve
-      x = c(0, xcart, seq(xcart, 1, (1-xcart)/50), 1)
-      # the y coordinates for the curve
-      y = c(0, ycart, sqrt(1-(seq(xcart, 1, (1-xcart)/50)^2)), 0) 
-      # drawing the polygon
-      polygon(x, y, col='skyblue', border = NA)
-    }
-    
-    else if(input$psuccess <=.5){
-      # the curve for the first section but for a full 25%
-      # replaced xcart with 0 and ycart with 1 because 25% of the circle (rotating like polar coordinates) is at point (0,1)
-      x = c(0,0,seq(0,1,(1-0)/50),1)
-      y = c(0,1, sqrt(1-(seq(0, 1, (1-0)/50)^2)), 0) 
-      polygon(x, y, col='skyblue', border = NA)
-      
-      # the curve for the second section 
-      x = c(xcart, seq(xcart, 0, (1-xcart)/50), -.00000000001, 0)
-      y = c(ycart, sqrt(1-(seq(xcart, 0, (1-xcart)/50)^2)), .999999999, 0) 
-      polygon(x, y, col='skyblue', border = NA)
-    }
-    
-    else if(input$psuccess <= .75){
-      
-      # the curve for the first section but for a full 25%
-      # replaced xcart with 0 and ycart with 1 because 25% of the circle (rotating like polar coordinates) is at point (0,1)
-      x = c(0,0,seq(0, 1, 1/50), 1)
-      y = c(0,1, sqrt(1-(seq(0, 1, 1/50)^2)), 0) 
-      polygon(x, y, col='skyblue', border = NA)
-      
-      # the curve for the second section but for a full 50%
-      # replaced xcart with 1 and ycart with 0 because 50% of the circle (rotating like polar coordinates) is at point (-1,0)
-      x = c(-1, seq(-1, 0, 1/50), 0, 0)
-      y = c(0, sqrt(1-(seq(-1, 0, 1/50)^2)), 1, 0) 
-      polygon(x, y, col='skyblue', border = NA)
-      
-      # the curve for the third section of the pie
-      x = c(-1, seq(-1, xcart, abs(1-xcart)/50), xcart, 0)
-      # using negative sqrt because we are drawing the bottom half of a circle
-      y = c(0, -sqrt(1-(seq(-1, xcart, abs(1-xcart)/50)^2)), ycart, 0) 
-      polygon(x, y, col='skyblue', border = NA)
-    }
-    else if(input$psuccess < 1){
-      
-      # the curve for the first section but for a full 25%
-      # replaced xcart with 0 and ycart with 1 because 25% of the circle (rotating like polar coordinates) is at point (0,1)
-      x = c(0,0,seq(0, 1, 1/50), 1)
-      y = c(0,1, sqrt(1-(seq(0, 1, 1/50)^2)), 0) 
-      polygon(x, y, col='skyblue', border = NA)
-      
-      # the curve for the second section but for a full 50%
-      # replaced xcart with 1 and ycart with 0 because 50% of the circle (rotating like polar coordinates) is at point (-1,0)
-      x = c(-1, seq(-1, 0, 1/50), 0, 0)
-      y = c(0, sqrt(1-(seq(-1, 0, 1/50)^2)), 1, 0) 
-      polygon(x, y, col='skyblue', border = NA)
-      
-      # the curve for the third section but for a full 75%
-      # replaced the xcart with 0 and ycart with 1 because 75% of the circle is at point (0,-1)
-      x = c(-1, seq(-1, 0, 1/50), 0, 0)
-      # using negative sqrt because we are drawing the bottom half of a circle
-      y = c(0, -sqrt(1-(seq(-1, 0, 1/50)^2)), -1, 0) 
-      polygon(x, y, col='skyblue', border = NA)
-      
-      # the curve for the fourth section of the pie
-      x = c(-1, seq(-1, xcart, abs(1-xcart)/50), xcart, 0)
-      # using negative sqrt because we are drawing the bottom half of a circle
-      y = c(0, -sqrt(1-(seq(-1, xcart, abs(1-xcart)/50)^2)), ycart, 0) 
-      polygon(x, y, col='skyblue', border = NA)
-    }
-    else {
-      
-      # the curve for the first section but for a full 25%
-      # replaced xcart with 0 and ycart with 1 because 25% of the circle (rotating like polar coordinates) is at point (0,1)
-      x = c(0,0,seq(0, 1, 1/50), 1)
-      y = c(0,1, sqrt(1-(seq(0, 1, 1/50)^2)), 0) 
-      polygon(x, y, col='skyblue', border = NA)
-      
-      # the curve for the second section but for a full 50%
-      # replaced xcart with 1 and ycart with 0 because 50% of the circle (rotating like polar coordinates) is at point (-1,0)
-      x = c(-1, seq(-1, 0, 1/50), 0, 0)
-      y = c(0, sqrt(1-(seq(-1, 0, 1/50)^2)), 1, 0) 
-      polygon(x, y, col='skyblue', border = NA)
-      
-      # the curve for the third section but for a full 75%
-      # replaced the xcart with 0 and ycart with 1 because 75% of the circle is at point (0,-1)
-      x = c(-1, seq(-1, 0, 1/50), 0, 0)
-      # using negative sqrt because we are drawing the bottom half of a circle
-      y = c(0, -sqrt(1-(seq(-1, 0, 1/50)^2)), -1, 0) 
-      polygon(x, y, col='skyblue', border = NA)
-      
-      # the curve for the fourth section of the pie
-      x = c(0, seq(0, 1, abs(1-0)/50), 1, 0)
-      # using negative sqrt because we are drawing the bottom half of a circle
-      y = c(-1, -sqrt(1-(seq(0, 1, abs(1-0)/50)^2)), 0, 0) 
-      polygon(x, y, col='skyblue', border = NA)
-    }
+    # shading the section of the circle representing P(success) 
+    polygon(c(0,xcart[1:(input$psuccess*1000+2)]), c(0,ycart[1:(input$psuccess*1000+2)]), col='skyblue', border = NA)
     
     # drawing a circle using curve()
-    curve(sqrt(1-x^2), from = -1, to = 1, n = 200, add = TRUE, xlab = "")
-    curve(-sqrt(1-x^2), from = -1, to = 1, n = 200, add = TRUE, xlab = "")
-    
-    # drawing the random lines with theta between 0 and 2pi x = rcos(data) and y = rsin(data) *data is theta*
-    segments(0, 0, cos(data), sin(data))
-    })
+    curve(sqrt(1-x^2), from = -1, to = 1, n = 200, add = TRUE, xlab = "", lwd = 2)
+    curve(-sqrt(1-x^2), from = -1, to = 1, n = 200, add = TRUE, xlab = "", lwd = 2)  
+    segments(0, 0, cos(data[1:input$n]), sin(data[1:input$n]))
+  })
   
   # Generates onesimTable and allows it to be reactive
   output$onesimTable <- renderTable({
-    
-    # getting the spinner data
-    unifdata = myData()
-    unifdata = data.frame(c(unifdata))
-    unifdata = unifdata[1:input$n,]
-    
     # determining if each part is a success or failure
-    success = data.frame(c(sum(unifdata <= input$psuccess)))
-    failure = data.frame(c(input$n - sum(unifdata <= input$psuccess)))
+    success = sum(data[1:input$n] <= input$psuccess*2*pi)
+    failure = input$n - success
     
-    # binding the successes and failures together to display them in simulationtable
-    simulationtable = cbind(format(success, nsmall = 0),format(failure, nsmall = 0))
-    names(simulationtable)[1] <- "Success"
-    names(simulationtable)[2] <- "Failure"
-    simulationtable
+    # binding the successes and failures together to display them in onetable
+    onetable = cbind(success, failure)
+    colnames(onetable) <- c("Success", "Failure")
+    onetable
   })
   
   # Generates remainingTable and allows it to be reactive
   output$remainingTable <- renderTable({
-    
-    # getting the binomial data
-    data = binData()
-    data = data.frame(c(data))
-    
     # displaying the successes and failures and displaying them in situationtable
-    success = data[0:(input$sims - 1),]
-    failure = data.frame(c(input$n - success))
-    simulationtable = cbind(format(success, nsmall = 0), format(failure, nsmall = 0))
-    names(simulationtable)[1] <- "Success"
-    names(simulationtable)[2] <- "Failure"
+    simsuccess = bindata[1:(input$sims),]
+    simfailure = input$n - simsuccess
+    simulationtable = cbind(simsuccess, simfailure)
+    colnames(simulationtable) <- c("Success", "Failure")
     simulationtable
   })
   
   # Generates distPlot and allows it to be reactive
   output$histPlot <- renderPlot({
-    
     # getting the data displayed in the spinner
-    unifdata = myData()
-    unifdata = data.frame(c(unifdata))
+    unifdata = data.frame(data)
     unifdata = unifdata[1:input$n,]
     
     # counting the number of successes in the spinner
@@ -264,7 +112,7 @@ server <- function(input, output) {
     if (input$sims > 1){
       
       # getting the remaining data for the simulation
-      remainingsuccesses = binData()
+      remainingsuccesses = bindata
       remainingsuccesses = data.frame(c(remainingsuccesses))
       remainingsuccesses = data.frame(c(remainingsuccesses[0:(input$sims - 1),]))
       names(remainingsuccesses)[1] <- "Success"
